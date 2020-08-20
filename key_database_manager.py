4fe851bd82b8babead9f69ae4b8e6c2833515754
Lily <lily.mosquitoes@gmail.com>
2020-08-19 23:08:20 -0300

    first commit

---
+++ key_database_manager.py
@@ -0,0 +1,106 @@
+import sys
+import configparser
+from PyQt5 import QtWidgets, uic, QtGui
+from ui_files.main_window import Ui_MainWindow
+from models.model_dataset import ModelDataset
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, user, passwd, *args, obj=None, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        # database connection
+        self.db = ModelDataset(user, passwd)
+        self.db_species = self.db.list_species()
+        self.db_couplets = self.db.list_couplets()
+        #
+        self.select_species = self.fetch_select_species()
+        self.select_couplets = self.fetch_select_couplets()
+        #
+        self.UiComponents()
+
+    def closeEvent(self, event):
+        self.db.connection.close()
+        event.accept()
+
+
+    def UiComponents(self):
+        self.comboBox_couplet.addItems(self.select_couplets)
+        self.comboBox_species.addItems(self.select_species)
+        self.comboBox_status.addItems(['0', '1', '01', 'NA'])
+        #
+        self.onJumpPress()
+        #
+        self.pushButton_jump.pressed.connect(self.onJumpPress)
+        #
+        self.pushButton_nextCouplet.pressed.connect(lambda: self.onCouplet(1))
+        self.pushButton_previousCouplet.pressed.connect(lambda: self.onCouplet(-1))
+        #
+        self.pushButton_nextSpecies.pressed.connect(lambda: self.onSpecies(1))
+        self.pushButton_previousSpecies.pressed.connect(lambda: self.onSpecies(-1))
+        #
+        ### MOST IMPORTTANT BUTTON: UPDATE FUNCTION
+        self.pushButton_change.pressed.connect(self.onChange)
+
+    def fetch_select_couplets(self):
+        file = open('select/COUPLETS.txt').read().strip().split('\n')
+        select_couplets = [c for c in self.db_couplets if c in file]
+        return select_couplets
+
+    def fetch_select_species(self):
+        file = open('select/SPECIES.txt').read().strip().split('\n')
+        select_species = [s for s in self.db_species if s in file]
+        return select_species
+
+    def onJumpPress(self):
+        self.c_couplet = str(self.comboBox_couplet.currentText())
+        self.c_species = str(self.comboBox_species.currentText())
+        #
+        zero_text, one_text = self.db.show_couplet(self.c_couplet)
+        state = self.db.show_state(self.c_species, self.c_couplet)
+        #
+        self.label_couplet.setText('Current couplet: {}'.format(self.c_couplet))
+        self.label_zero.setText(zero_text)
+        self.label_one.setText(one_text)
+        self.label_species.setText(self.c_species)
+        self.label_status.setText('Current status: {}'.format(state or 'NULL'))
+
+    def onCouplet(self, add):
+        c_couplet_index = self.select_couplets.index(self.c_couplet)
+        n_couplet_index = c_couplet_index + add
+        self.comboBox_couplet.setCurrentIndex(n_couplet_index)
+        self.onJumpPress()
+
+    def onSpecies(self, add):
+        c_species_index = self.select_species.index(self.c_species)
+        n_species_index = c_species_index + add
+        self.comboBox_species.setCurrentIndex(n_species_index)
+        self.onJumpPress()
+
+    def onChange(self):
+        # UPDATE db
+        new_state = str(self.comboBox_status.currentText())
+        self.db.update(self.c_species, new_state, self.c_couplet)
+        # confirm UPDATE
+        state = self.db.show_state(self.c_species, self.c_couplet)
+        self.label_status.setText('Current status: {}'.format(state or 'NULL'))
+
+
+###
+def read_conf():
+    config = configparser.ConfigParser()
+    config.read('conf/conf')
+    user = config['mosquito database']['User']
+    passwd = config['mosquito database']['Password']
+    return user, passwd
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    #
+    user, passwd = read_conf()
+    #
+    main = MainWindow(user, passwd)
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
---
+++ ta_key_database_manager.py
@@ -0,0 +1,151 @@
+import curses
+import signal
+import sys
+import configparser
+from models.model_dataset import ModelDataset
+
+
+class Database(object):
+    def __init__(self, user, passwd):
+        self.db = ModelDataset(user, passwd)
+        self.db_species = self.db.list_species()
+        self.db_couplets = self.db.list_couplets()
+        #
+        self.select_species = self.fetch_select_species()
+        self.select_couplets = self.fetch_select_couplets()
+        #
+        self.sp_index = 0
+        self.cp_index = 0
+
+    def fetch_select_couplets(self):
+        file = open('select/COUPLETS.txt').read().strip().split('\n')
+        select_couplets = [c for c in self.db_couplets if c in file]
+        return select_couplets
+
+    def fetch_select_species(self):
+        file = open('select/SPECIES.txt').read().strip().split('\n')
+        select_species = [s for s in self.db_species if s in file]
+        return select_species
+
+
+def read_key(value):
+    #
+    r = None
+    #
+    curses_keys = {
+    'KEY_DOWN': curses.KEY_DOWN,
+    'KEY_UP': curses.KEY_UP,
+    'KEY_LEFT': curses.KEY_LEFT,
+    'KEY_RIGHT': curses.KEY_RIGHT,
+    'KEY_BACKSPACE': curses.KEY_BACKSPACE,
+    'KEY_ENTER': ord('\n')
+    }
+    try:
+        if value in curses_keys.keys():
+            r = curses_keys[value]
+        else:
+            r = ord(value)
+    except TypeError:
+        pass
+    #
+    return r
+
+def read_conf():
+    #
+    config = configparser.ConfigParser()
+    config.read('conf/conf')
+    user = config['mosquito database']['User']
+    passwd = config['mosquito database']['Password']
+    keybindings = {
+        'NextCouplet': read_key(config['ta keybindings']['NextCouplet']) or 's',
+        'PreviousCouplet': read_key(config['ta keybindings']['PreviousCouplet']) or 'a',
+        'NextSpecies': read_key(config['ta keybindings']['NextSpecies']) or 'x',
+        'PreviousSpecies': read_key(config['ta keybindings']['PreviousSpecies']) or 'z',
+        'Update': read_key(config['ta keybindings']['Update']) or '\n',
+        'Cancel': read_key(config['ta keybindings']['Cancel']) or 'c',
+        'Quit': read_key(config['ta keybindings']['Quit'] or 'q')
+    }
+
+    return user, passwd, keybindings
+
+def main(term):
+    #
+    def signal_handler(sig, frame):
+        ## code adapted from: https://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
+        print('You pressed Ctrl+C!')
+        db.db.connection.close()
+        sys.exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    #
+    term.clear()
+    #
+    # reading config:
+    user, passwd, keybindings = read_conf()
+    #
+    db = Database(user, passwd)
+    #
+    while True:
+        term.clear()
+        #
+        couplet = db.select_couplets[db.cp_index]
+        zero_text, one_text = db.db.show_couplet(couplet)
+        species = db.select_species[db.sp_index]
+        status = db.db.show_state(species, couplet)
+        #
+        term.addstr("""
+        current couplet: {}
+
+            0. {}
+
+            1. {}
+
+
+        current species: {}
+
+            status: {}
+
+        """.format(couplet, zero_text, one_text, species, status))
+        term.refresh()
+        y, x = term.getyx()
+        #
+        key = term.getch()
+        #
+        if key == keybindings['NextCouplet']:
+            if db.cp_index < len(db.select_couplets):
+                db.cp_index += 1
+        elif key == keybindings['PreviousCouplet']:
+            if db.cp_index > 0:
+                db.cp_index -= 1
+        elif key == keybindings['NextSpecies']:
+            if db.sp_index < len(db.select_species):
+                db.sp_index += 1
+        elif key == keybindings['PreviousSpecies']:
+            if db.sp_index > 0:
+                db.sp_index -= 1
+        elif key == keybindings['Update']:
+            term.addstr(y, x, 'type a new value to edit the database: ')
+            curses.echo()
+            new_status = term.getstr().decode('utf-8').upper()
+            curses.noecho()
+            if new_status in ['0', '1', '01', '10', 'NA']:
+                term.addstr(y+1, x, 'new status: {}'.format(new_status))
+                term.addstr(y+2, x, "press any key to confirm, 'c' to cancel")
+                confirm = term.getch()
+                if confirm == keybindings['Cancel']:
+                    term.addstr(y+3, x, 'action cancelled, press any key to continue')
+                else:
+                    db.db.update(species, new_status, couplet)
+                    term.addstr(y+3, x, 'change confirmed, press any key to continue')
+            else:
+                term.addstr(y+1, x, 'illegal status: {}'.format(new_status))
+                term.addstr(y+2, x, "please type '0', '1', '01' or 'NA'")
+            term.getch()
+        elif key == keybindings['Quit']:
+            break
+
+    db.db.connection.close()
+
+
+if __name__ == '__main__':
+    curses.wrapper(main)
