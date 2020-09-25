3681e7cbc2a478292f855f77b81eae2374c2e79d
Lily <lily.mosquitoes@gmail.com>
2020-09-25 18:45:40 -0300

    widened the config window a tad ¯\_(ツ)_/¯

--- ui_files/config_window.py
+++ ui_files/config_window.py
@@ -11,7 +11,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(311, 338)
+        Dialog.resize(323, 338)
         self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName("verticalLayout")
         self.label_message = QtWidgets.QLabel(Dialog)
